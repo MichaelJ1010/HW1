7680dee30aa4742d389231d8b11a50d0e05e9fab
Michael J <30031129@cc.peralta.edu>
2024-09-22 16:33:00 -0700

    added while loop to find binary order

--- NumberToBinary.cpp
+++ NumberToBinary.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main(){
+    int number;
+    cout << "Please write a positive integer " << endl;
+    cin >> number;
 
+    while(number != 0){
+        if((number % 2 )== 0){
+            cout << "0";
+            number /= 2;
+        }else{
+            cout << "1";
+            number /= 2;
+        }
+    }
+    
     return 0;
 }
