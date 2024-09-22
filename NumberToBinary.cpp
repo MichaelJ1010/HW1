742c6a4e381e273d47e49cb42b020a17b095ecae
Michael J <30031129@cc.peralta.edu>
2024-09-22 16:54:31 -0700

    ensured binary will be in groups of 4

--- NumberToBinary.cpp
+++ NumberToBinary.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(){
     int number;
+    int countFours;
     do{
     cout << "Please write a positive integer " << endl;
     cin >> number;
@@ -11,14 +12,20 @@ int main(){
 
     cout << number << " in binary: ";
 
-    while(number != 0){
+    while(number != 0){  //calculates the 0's and 1's for the number
         if((number % 2 )== 0){
             cout << "0";
             number /= 2;
+            countFours ++;
         }else{
             cout << "1";
             number /= 2;
+            countFours++;
         }
+    }
+    while((countFours % 4) != 0){
+        cout << "0";
+        countFours++;
     }
     
     return 0;
